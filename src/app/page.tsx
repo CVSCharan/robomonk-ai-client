d2a2bdd06bae1dc36baeb712df8264c3ca87001e
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-02-12 05:42:11 +0530

    New Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
+import { Instagram } from "@mui/icons-material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Home() {
   const router = useRouter();
@@ -81,6 +84,47 @@ export default function Home() {
         </div>
       </section>
       <footer className={styles.footerContainer}>
+        <div className={styles.socialMediaIcons}>
+          <a
+            href="https://www.instagram.com/robomonk.ai/?igsh=bnh1M2owOGRiOTRl#"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <Instagram className={styles.icon} fontSize="large" />
+          </a>
+          <a
+            href="https://wa.me/qr/K5ILKPFNLG76J1"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Facebook"
+          >
+            <WhatsAppIcon className={styles.icon} fontSize="large" />
+          </a>
+          <a
+            href="https://www.threads.net/@robomonk.ai?invite=0"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Twitter"
+          >
+            <Image
+              src={"/images/threads.png"}
+              alt={"Threads social media icon"}
+              height={100}
+              width={100}
+              priority
+              className={styles.icon}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/robomonk-ai/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <LinkedInIcon className={styles.icon} fontSize="large" />
+          </a>
+        </div>
         <h2 className={styles.footerHeading}>
           © 2025 All rights reserved - Robomonk Technologies Pvt. Ltd.
         </h2>
